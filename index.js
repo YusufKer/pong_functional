389c5552ff6aca065353c2d3975f1f023a3fbd53
yusuf <yusuf@elemental.co.za>
2023-02-14 09:48:32 +0200

    paddle collision logic added. win and lose conditions added

--- index.js
+++ index.js
@@ -6,26 +6,39 @@ ctx.fillRect(0,0,canvas.width,canvas.height);
 
 const paddle_width = 10;
 const paddle_height = 100;
-const paddle_x_position = 10;
-let paddle_y_position = 300;
+const player_paddle_x_position = 10;
+const pc_paddle_x_position = canvas.width - paddle_width - 10;
+
+let player_paddle_y_position = 300;
+let pc_paddle_y_position = 400;
 
 const ball_width = 10;
 const ball_height = 10;
-let ball_x_position = 100;
+let ball_x_position = 300;
 let ball_y_position = 40;
 
 let ball_x_direction = -10;
 let ball_y_direction = 10
 
-function animatePaddle(){
+function drawPlayerPaddle(){
     ctx.fillStyle = "black";
-    ctx.fillRect(paddle_x_position,paddle_y_position,paddle_width,paddle_height);
+    ctx.fillRect(player_paddle_x_position,player_paddle_y_position,paddle_width,paddle_height);
 }
 function clearPaddle(){
     ctx.fillStyle = "grey";
-    ctx.fillRect(paddle_x_position,paddle_y_position,paddle_width,paddle_height);
+    ctx.fillRect(player_paddle_x_position,player_paddle_y_position,paddle_width,paddle_height);
 }
-function animateBall(direction = -1){
+
+function drawPCPaddle(){
+    ctx.fillStyle = "black";
+    ctx.fillRect(pc_paddle_x_position,pc_paddle_y_position,paddle_width,paddle_height);
+}
+function clearPCPaddle(){
+    ctx.fillStyle = "grey";
+    ctx.fillRect(pc_paddle_x_position,pc_paddle_y_position,paddle_width,paddle_height);
+}
+
+function animateBall(){
     ctx.fillStyle = "white";
     ctx.fillRect(ball_x_position,ball_y_position,ball_width,ball_height);
 }
@@ -34,61 +47,89 @@ function clearBall(){
     ctx.fillRect(ball_x_position,ball_y_position,ball_width,ball_height);
 }
 function checkCollision(){
-    // CHECK IF BALL HITS BAT
-    if(ball_x_position <= paddle_x_position + ball_width){
-        if(ball_y_position >= paddle_y_position && ball_y_position <= paddle_y_position + paddle_height){
-            stopBall();
-            ball_x_direction = -ball_x_direction;
-            ball_y_direction = ball_y_direction;
-            let moveBall = setInterval(()=>{
-                clearBall()
-                ball_x_position += ball_x_direction;
-                ball_y_position -= ball_y_direction;
-                animateBall();
-            },300)
-        }else{
-            stopBall();
-        }
-    }
     // CHECK IF BALL HITS TOP
     if(ball_y_position === 0 && ball_y_position <= 0 ){
+        ball_y_direction = - ball_y_direction;
+    }
+    // CHECK IF BALL HITS LEFT
+    if(ball_x_position === 0){
         stopBall();
-        ball_y_direction = -ball_y_direction;
-        let moveBall = setInterval(()=>{
-            clearBall()
-            ball_x_position += ball_x_direction;
-            ball_y_position -= ball_y_direction;
-            animateBall();
-        },300)
+        alert("You lose!");
+    }
+    // CHECK IF BALL HITS BOTTOM
+    if(ball_y_position === canvas.height - ball_height){
+        ball_y_direction = - ball_y_direction;
+    }
+    // CHECK IF BALL HITS RIGHT
+    if(ball_x_position === canvas.width - ball_width){
+        stopBall();
+        alert("You win!");
+    }
+    // CHECK IF BALL HITS LEFT PADDLE
+    if(ball_x_position === (player_paddle_x_position + paddle_width)  && (ball_y_position >= player_paddle_y_position && ball_y_position <= (player_paddle_y_position + paddle_height))){
+        ball_x_direction = -ball_x_direction;
+    }
+    // // CHECK IF BALL HITS RIGHT PADDLE
+    if(ball_x_position === (pc_paddle_x_position - paddle_width) && (ball_y_position >= pc_paddle_y_position && ball_y_position <= (pc_paddle_y_position + paddle_height))){
+        ball_x_direction = -ball_x_direction;
     }
 }
 
-animatePaddle();
-animateBall();
-
-window.addEventListener("keypress", e =>{
-    if(e.key === "w"){
-        if(paddle_y_position === 0) return
-        clearPaddle();
-        paddle_y_position -= 10;
-        animatePaddle();
-    }else if(e.key === "s"){
-        if(paddle_y_position === canvas.height - paddle_height) return
-        clearPaddle();
-        paddle_y_position += 10;
-        animatePaddle();
-    }else return;
-})
-
-let moveBall = setInterval(()=>{
+function step(){
     clearBall()
     ball_x_position += ball_x_direction;
     ball_y_position -= ball_y_direction;
     animateBall();
     checkCollision();
-},300)
-
-function stopBall(){
-    clearInterval(moveBall)
 }
 
+function stopBall(){
+    clearInterval(moveBall);
+}
+drawPlayerPaddle();
+drawPCPaddle();
+animateBall();
+let moveBall = setInterval(step,75);
+// stopBall();
+
+let movePCPaddle = setInterval(()=>{
+    if(ball_y_position > pc_paddle_y_position){
+        clearPCPaddle();
+        pc_paddle_y_position += 10;
+        drawPCPaddle();
+    }else{
+        clearPCPaddle();
+        pc_paddle_y_position -= 10;
+        drawPCPaddle();
+    }
+},100)
+clearInterval(movePCPaddle);
+
+window.addEventListener("keypress", e =>{
+    switch(e.key){
+        case "w" : 
+            if(player_paddle_y_position === 0) return;
+            clearPaddle();
+            player_paddle_y_position -= 10;
+            drawPlayerPaddle();
+            break;
+        case "s" : 
+            if(player_paddle_y_position === canvas.height - paddle_height) return;
+            clearPaddle();
+            player_paddle_y_position += 10;
+            drawPlayerPaddle();
+            break;
+        case "8" : 
+            if(pc_paddle_y_position === 0) return;
+            clearPCPaddle();
+            pc_paddle_y_position -= 10;
+            drawPCPaddle();
+            break;
+        case "2" : 
+            if(pc_paddle_y_position === canvas.height - paddle_height) return;
+            clearPCPaddle();
+            pc_paddle_y_position += 10;
+            drawPCPaddle();
+            break;
+    }
+})
